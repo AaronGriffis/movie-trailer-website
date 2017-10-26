20885df9119db9e36549685dec937ffa18a96e79
aarongriffis <aarongriffis@hotmail.com>
2017-10-26 10:31:59 -0700

    Complete Project 1

--- README.md
+++ README.md
@@ -3,13 +3,13 @@
 ## Content Proiority Outline
 1. Title
 2. Trailer
-3. Release Date
-4. Movie Info
+3. Movie Info
+   * Rating
+   * Runtime
+   * Genres
    * Description
-   * Director
    * Actors
-5. Imagery
-6. Ratings and Reviews
+4. Imagery
 
 ## Art Direction
 * Whimsical, adventurous, colorful, bright and cheery, fast-paced, humerous, scenic, romantic, fun
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
-//Lettering
-$('h1').lettering();
+//Arctext
+setArcText($(window).width());
+
+$(window).resize(function() {
+   setArcText($(window).width());
+});
 
 //Media Element Player
 $('video').mediaelementplayer();
+
+function setArcText(radius) {
+   console.log(radius);
+   $('#curved-trailer').replaceWith($('<h1 id="curved-trailer">MOVIE TRAILER</h1>'));
+   
+   $('#curved-trailer').fitText(2.5);
+   $('#curved-trailer').arctext({
+      radius: radius/2
+   });
+}
